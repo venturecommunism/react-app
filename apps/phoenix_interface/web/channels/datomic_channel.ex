7d29516c72fd853de475b99602a6079650609d7e
anonymous <anonymous>
2017-06-15 21:03:12 +0000

    untested port of the transaction code

--- apps/phoenix_interface/web/channels/datomic_channel.ex
+++ apps/phoenix_interface/web/channels/datomic_channel.ex
@@ -17,7 +17,30 @@ defmodule PhoenixInterface.DatomicChannel do
 
   def handle_in("new:msg", %{"body" => %{"data" => data, "meta" => meta}, "user" => user}, socket) do
     IO.inspect "transact"
-    {:noreply, socket}
+    msg = %{"body" => %{"data" => data, "meta" => meta}, "user" => user}
+
+    IO.inspect msg
+    IO.puts 'data'
+    IO.inspect data
+    some_data = ParseDatascriptTransaction.first(data)
+    IO.puts 'some_data'
+    IO.inspect some_data
+
+    data_to_add = """
+      [ { :db/id #db/id[:db.part/db]
+          :db/ident :person/name
+          :db/valueType :db.type/string
+          :db/cardinality :db.cardinality/one
+          :db/doc \"A person's name\"
+          :db.install/_attribute :db.part/db}]
+    """
+    {:ok, transaction_result} = DatomicGenServer.transact(DatomicGenServerLink, some_data, [:options, {:client_timeout, 100_000}])
+
+    IO.puts transaction_result
+
+    # push socket, "join", %{status: "connected"}
+    # broadcast! socket, "new:msg", %{user: user, body: %{"syncpoint": false, "user": user}}
+    {:reply, {:ok, %{msg: %{"syncpoint": false, "user": user}}}, assign(socket, :user, user)}
   end
 
   def handle_in("new:msg", msg, socket) do
