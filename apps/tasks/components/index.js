8adb1880be857844eea4a1fdf999d1dee7bcae64
anonymous <anonymous>
2017-12-05 06:48:46 -0500

    doing a pull query in its own pull container defined externally

--- apps/tasks/components/index.js
+++ apps/tasks/components/index.js
@@ -1,15 +1,21 @@
 import React from 'react'
 
+import PullDataContainer from '../containers/pulldatacontainer'
 import DataContainer from '../containers/tasksdatacontainer'
 import ActionsMapper from '../../core/containers/actionsmapper'
 
+import pullProjectsQuery from '../queries/pullprojects'
 import projectsQuery from '../queries/projects'
+
+import PullProjectsComponent from './pullprojects'
 import ProjectsComponent from './projects'
 
+const PullProjects = PullDataContainer(PullProjectsComponent)
 const Projects = DataContainer(ActionsMapper('projects', ProjectsComponent))
 
 const Tasks = () => (
   <div>
+    <PullProjects pullquery={pullProjectsQuery} />
     <Projects query={projectsQuery} />
   </div>
 )
---
+++ apps/tasks/components/pullprojects.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const PlainResultComponent = ({ result }) => (
+  <div>
+    <h3>Pull Projects </h3>
+      <h4>result: {JSON.stringify(result)}</h4>
+  </div>
+)
+
+export default PlainResultComponent
