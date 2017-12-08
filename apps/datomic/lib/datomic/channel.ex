3f712a9ed184daf004a15f494584b4fc71146b16
anonymous <anonymous>
2017-12-08 12:17:42 +0000

    this commit (not the last) removes queries. not sure if they're useful

--- apps/datomic/lib/datomic/channel.ex
+++ apps/datomic/lib/datomic/channel.ex
@@ -20,23 +20,7 @@ defmodule Datomic.Channel do
   end
 
   def sync(latest_tx, socket) do
-#    query = "[:find ?e ?v ?tx ?op :where [?e :db/doc ?v ?tx ?op]]"
-#    query = "[:find (count ?tx) :in ?log ?t1 ?t2 :where [(tx-ids ?log ?t1 ?t2) [?tx ...]]]"
-#    query = "[:find ?e ?a ?v ?tx ?op :in ?log ?tx :where [(tx-data ?log ?tx)[[?e ?a ?v _ ?op]]]]"
-
-#    query = "[:find (count ?tx) ?tx :in ?log ?t1 ?t2 :where [(tx-ids ?log ?t1 ?t2) [?tx ...]]]"
-
-#    query = "[:find ?e ?a ?v ?tx ?op :in ?log ?t1 ?t2 :where [(tx-ids ?log ?t1 ?t2) [?tx ...]] [(tx-data ?log ?tx) [[?e ?a ?v _ ?op]]]]"
     query = "[:find ?e ?a ?v ?tx ?op :in ?log ?t1 :where [(tx-ids ?log ?t1 nil) [?tx ...]] [(tx-data ?log ?tx) [[?e ?a ?v _ ?op]]]]"
-
-#    query = ":eavt"
-#    query = "[:find ?e ?aname ?v ?t ?added
-#              :in $ [[?e ?a ?v ?t ?added]]
-#              :where [?a :db/ident ?aname]]"
-
-#    query = "[:find ?e ?aname ?v ?tx ?op :where [?e ?a ?v ?tx ?op] [?a :db/ident ?aname]]"
-
-
     {:ok, edn} = DatomicGenServer.qlog(DatomicGenServerLink, query, latest_tx - 1 , [], [:options, {:client_timeout, 100_000}])
     IO.puts "test"
     grouped_tx = Datomic.TransactionLogQueryLogger.parse(edn) |> Enum.group_by( fn(x) -> x["tx"] end )
