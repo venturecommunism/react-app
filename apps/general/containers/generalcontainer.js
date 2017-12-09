47b99b3ec07004f58ece410dccb43e18cbb33591
anonymous <anonymous>
2017-12-09 10:49:31 -0500

    breaking change but contains code for a recursive muiltidimensional sort. needs to be modified to handle arrays within arrays and different DESC and ASC and structure

--- apps/general/containers/generalcontainer.js
+++ apps/general/containers/generalcontainer.js
@@ -10,41 +10,93 @@ const dataComposer = ({ context, componentid }, onData) => {
     [:find ?query
      :where [?e "query" ?query]
             [?e "componentid" "${componentid}"]]`
+
   const cArgs = [cQuery, db]
   const query = datascript.q(...cArgs)[0][0]
   const qArgs = [query, db]
 
-  try {
-    function generalsort(array, index) {
-      function sortfunction(a,b) {
-        if (index[0] >= 1) {
-alert('POSITIVE SORT')
-          return a[index[0]] > b[index[0]]
-        } else {
-alert('NEGATIVE SORT')
-          // do reverse sorting if the number is negative
-          index[0] = Math.abs(index[0])
-          return b[index[0]] > a[index[0]]
+try {
+
+  if( typeof helper == 'undefined' ) {
+    var helper = { } ;
+  }
+
+  helper.arr = {
+    /**
+     * Function to sort multidimensional array
+     * 
+     * param {array} [arr] Source array
+     * param {array} [columns] List of columns to sort
+     * param {array} [order_by] List of directions (ASC, DESC)
+     * returns {array}
+     */
+    multisort: function(arr, columns, order_by) {
+      if(typeof columns == 'undefined') {
+        columns = []
+        for(x=0;x<arr[0].length;x++) {
+          columns.push(x);
         }
       }
-      if (index.length <= 1) {
-alert('index length 1 or less')
-        return array.sort(sortfunction)
+
+      if(typeof order_by == 'undefined') {
+        order_by = []
+        for(x=0;x<arr[0].length;x++) {
+          order_by.push('ASC');
+        }
       }
-      else {
-alert('index length greater or equal to 1')
-        var newarray = array.sort(sortfunction)
-alert(array)
-alert(newarray)
-        return generalsort(newarray, index.slice(1, index.length)) 
+
+      function multisort_recursive(a,b,columns,order_by,index) {  
+        var direction = order_by[index] == 'DESC' ? 1 : 0;
+
+        var is_numeric = !isNaN(a[columns[index]]-b[columns[index]]);
+
+        var x = is_numeric ? a[columns[index]] : a[columns[index]].toLowerCase();
+        var y = is_numeric ? b[columns[index]] : b[columns[index]].toLowerCase();
+
+/*
+            if(!is_numeric) {
+                x = helper.string.to_ascii(a[columns[index]].toLowerCase(),-1),
+                y = helper.string.to_ascii(b[columns[index]].toLowerCase(),-1);
+            }
+*/
+        if(x < y) {
+          return direction == 0 ? -1 : 1;
+        }
+
+        if(x == y)  {
+          return columns.length-1 > index ? multisort_recursive(a,b,columns,order_by,index+1) : 0;
+        }
+
+        return direction == 0 ? 1 : -1;
       }
+
+      return arr.sort(function (a,b) {
+        return multisort_recursive(a,b,columns,order_by,0);
+      })
     }
-    var result = generalsort(datascript.q(...qArgs), [-1, -1])
-    onData(null, {result})
-  } catch (error) {
-    alert(error)
-    onData(null, {error})
   }
+
+  var peoples = [
+      {name: 'Jean', lastname: 'Rodrigues', points: 30},
+      {name: 'Sara', lastname: 'Hope', points: 30},
+      {name: 'Igor', lastname: 'Leroy', points: 25},
+      {name: 'Foo', lastname: 'Bar', points: 55}
+  ]
+
+  // sort this list by points, if points is equal, sort by name.
+  var result = helper.arr.multisort(peoples, ['points', 'name'], ['DESC','ASC'])
+  console.log(result)
+  // result = datascript.q(...qArgs)
+  onData(null, {result})
+} catch (error) {
+  alert(error)
+  onData(null, {error})
+}
+
+/*
+
+*/
+
 }
 
 export default (component) => composeAll(
