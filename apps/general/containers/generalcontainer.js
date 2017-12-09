945835875650485e36958d24b7fc67a37d1d3c96
anonymous <anonymous>
2017-12-09 14:12:26 -0500

    cleanup. sorts not fully functionaly but mostly

--- apps/general/containers/generalcontainer.js
+++ apps/general/containers/generalcontainer.js
@@ -21,44 +21,44 @@ const dataComposer = ({ context, componentid }, onData) => {
   // arguments for the actual query we want
   const qArgs = [query, db]
 
-// recursive multidimensional array sort being formed. TODO: change the structure to match datascript output (arrays all the way down)
-try {
+  // recursive multidimensional array sort being formed. TODO: change the structure to match datascript output (arrays all the way down)
+  try {
 
-  if( typeof helper == 'undefined' ) {
-    var helper = { } ;
-  }
+    if( typeof helper == 'undefined' ) {
+      var helper = { } ;
+    }
 
-  helper.arr = {
-    /**
-     * Function to sort multidimensional array
-     * 
-     * param {array} [arr] Source array
-     * param {array} [columns] List of columns to sort
-     * param {array} [order_by] List of directions (ASC, DESC)
-     * returns {array}
-     */
-    multisort: function(arr, columns, order_by) {
-      if(typeof columns == 'undefined') {
-        columns = []
-        for(x=0;x<arr[0].length;x++) {
-          columns.push(x);
+    helper.arr = {
+      /**
+       * Function to sort multidimensional array
+       *
+       * param {array} [arr] Source array
+       * param {array} [columns] List of columns to sort
+       * param {array} [order_by] List of directions (ASC, DESC)
+       * returns {array}
+       */
+      multisort: function(arr, columns, order_by) {
+        if(typeof columns == 'undefined') {
+          columns = []
+          for(x=0;x<arr[0].length;x++) {
+            columns.push(x);
+          }
         }
-      }
 
-      if(typeof order_by == 'undefined') {
-        order_by = []
-        for(x=0;x<arr[0].length;x++) {
-          order_by.push('ASC');
+        if(typeof order_by == 'undefined') {
+          order_by = []
+          for(x=0;x<arr[0].length;x++) {
+            order_by.push('ASC');
+          }
         }
-      }
 
-      function multisort_recursive(a,b,columns,order_by,index) {  
-        var direction = order_by[index] == 'DESC' ? 1 : 0;
+        function multisort_recursive(a,b,columns,order_by,index) {
+          var direction = order_by[index] == 'DESC' ? 1 : 0;
 
-        var is_numeric = !isNaN(a[columns[index]]-b[columns[index]]);
+          var is_numeric = !isNaN(a[columns[index]]-b[columns[index]]);
 
-        var x = is_numeric ? a[columns[index]] : a[columns[index]].toLowerCase();
-        var y = is_numeric ? b[columns[index]] : b[columns[index]].toLowerCase();
+          var x = is_numeric ? a[columns[index]] : a[columns[index]].toLowerCase();
+          var y = is_numeric ? b[columns[index]] : b[columns[index]].toLowerCase();
 
 /*
 // TODO: replace helper with something tha will work for non-numerics
@@ -67,54 +67,29 @@ try {
                 y = helper.string.to_ascii(b[columns[index]].toLowerCase(),-1);
             }
 */
-        if(x < y) {
-          return direction == 0 ? -1 : 1;
+          if(x < y) {
+            return direction == 0 ? -1 : 1;
+          }
+
+          if(x == y)  {
+            return columns.length-1 > index ? multisort_recursive(a,b,columns,order_by,index+1) : 0;
+          }
+
+          return direction == 0 ? 1 : -1;
         }
 
-        if(x == y)  {
-          return columns.length-1 > index ? multisort_recursive(a,b,columns,order_by,index+1) : 0;
-        }
-
-        return direction == 0 ? 1 : -1;
+        return arr.sort(function (a,b) {
+          return multisort_recursive(a,b,columns,order_by,0);
+        })
       }
-
-      return arr.sort(function (a,b) {
-        return multisort_recursive(a,b,columns,order_by,0);
-      })
     }
+
+    var result = helper.arr.multisort(datascript.q(...qArgs), [2, 0], ['DESC', 'ASC'])
+    onData(null, {result})
+  } catch (error) {
+    alert(error)
+    onData(null, {error})
   }
-
-  var peoples = [
-      {name: 'Jean', lastname: 'Rodrigues', points: 30},
-      {name: 'Sara', lastname: 'Hope', points: 30},
-      {name: 'Igor', lastname: 'Leroy', points: 25},
-      {name: 'Foo', lastname: 'Bar', points: 55}
-  ]
-
-  var peoples = [
-      ['Jean', 'Rodrigues', 30],
-      ['Sara', 'Hope', 30],
-      ['Igor', 'Leroy', 25],
-      ['Foo', 'Bar', 55]
-  ]
-
-  // sort this list by points, if points is equal, sort by name.
-
-  //  var result = helper.arr.multisort(peoples, ['points', 'name'], ['DESC','ASC'])
-
-  var result = helper.arr.multisort(peoples, [2, 0], ['DESC', 'ASC'])
-
-  var result = helper.arr.multisort(datascript.q(...qArgs), [2, 0], ['DESC', 'ASC'])
-
-  console.log(result)
-  // TODO: add back in the actual datascript query we want once multidimensional sorts work
-  // result = datascript.q(...qArgs)
-  onData(null, {result})
-} catch (error) {
-  alert(error)
-  onData(null, {error})
-}
-
 }
 
 export default (component) => composeAll(
