9c2a02128824d874543c6cf4a0c1efa64fc5131b
anonymous <anonymous>
2017-12-11 16:32:11 -0500

    not sure why this works. (it needs the weird console.log to get the count right). works for one record at least

--- apps/general/lib/badmapreduce.js
+++ apps/general/lib/badmapreduce.js
@@ -8,12 +8,14 @@ const badmapreduce = function (result, components) {
 
     for (var i = 0; i < result.length; i++) {
       var singlecomponent = new Array()
-//      for (var j = 0; j < result[i].length; j++) {
+      var l = 0
+      for (var j = 0; j < result[i].length; j++) {
         for (var k = 0; k < components._componentsparents.length; k++) {
+          console.log("ALPHABET", i, j, k, l)
           console.log("COMPONENTS", components._componentsparents[k])
-          components._componentsparents[k].componentstype == 'data' ? singlecomponent.push(result[i][k]) : singlecomponent.push(components._componentsparents[k].componentsname)
+          components._componentsparents[k].componentstype == 'data' ? singlecomponent.push(result[i][j + l]) && l++ : singlecomponent.push(components._componentsparents[k].componentsname) && (console.log(result[i][j + l]) && l++)
         }
-//      }
+      }
       filledcomponents.push(singlecomponent)
     }
 
