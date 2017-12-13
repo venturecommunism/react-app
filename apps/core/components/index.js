3b6208170c762b5c7f000218097d3f30e3a20c34
anonymous <anonymous>
2017-12-13 11:25:49 -0500

    may want to add all this back in to manage state at the highest level of routes and components

--- apps/core/components/index.js
+++ apps/core/components/index.js
@@ -5,10 +5,6 @@ import ActionsMapper from '../containers/actionsmapper'
 import RecursiveComponent from '../../cleanup/component'
 const Recursive = DataContainer(ActionsMapper('general', RecursiveComponent))
 
-import logo from '../../layout/components/images/fivepetal.svg'
-import '../../layout/components/styles/app.css'
-import '../../layout/components/styles/index.css'
-
 /**
 **  MAKING A NEW MODULE
 **  1. Import a new component and place it within the Root component
@@ -16,27 +12,7 @@ import '../../layout/components/styles/index.css'
 **/
 
 const Root = ({result}) => (
-  <div>{ !result ? <div>
-    <div className='App-header'>
-      {/* <StellarTestnet />
-      <TimeTracker />
-      <img src={logo} className='App-logo' alt='logo' /> */}
-      <h2>Under Construction</h2>
-    </div>
-    <p className='App-intro'>
-      To get started, edit <code>src/modules/core/components/index.js</code> and save to reload.
-    </p>
-    {/* this Recursive component is the main thing */}
-    <Recursive componentid={"rootcomponentcleanup"} />
- </div> : <div>
-    <div className='App-header'>
-      <img src={logo} className='App-logo' alt='logo' />
-    </div>
-    <p className='App-intro'>
-    </p>
-    </div>
-    }
-  </div>
+  <Recursive componentid={"rootcomponentcleanup"} />
 )
 
 export default Root
