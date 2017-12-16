a53e0330be1fb30a923e72eb98118907847c5795
Anonymous <you@example.com>
2017-12-16 16:50:23 +0000

    install script installs the elixir code but not datomic

--- install-server.sh
+++ install-server.sh
@@ -1,8 +1,8 @@
 run_it () {
 
 wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
-sudo apt-get update && sudo apt-get install -y git curl esl-erlang elixir build-essential erlang-dev
-echo "***** finished apt-get update and installed git, curl, erlang, elixir, build-essential (for cmake), erlang-dev"
+sudo apt-get update && sudo apt-get install -y git curl esl-erlang elixir build-essential
+echo "***** finished apt-get update and installed git, curl, erlang, elixir, build-essential (for cmake)"
 git clone https://github.com/venturecommunism/react-app.git
 echo "***** git cloned repository"
 cd react-app
