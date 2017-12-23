27bb1ab53e523f41ed4fcaa44b4f738a59f987d1
anonymous <you@example.com>
2017-12-23 16:50:51 +0000

    gives a more succinct acceptance testing API and takes screenshots on failure

--- apps/web_agent/test/support/integration_case.ex
+++ apps/web_agent/test/support/integration_case.ex
@@ -1,16 +1,19 @@
 defmodule WebAgent.IntegrationCase do
-
   use ExUnit.CaseTemplate
+  use Wallaby.DSL
 
   using do
     quote do
       use Wallaby.DSL
+
+      import WebAgent.TestHelpers
     end
   end
 
   setup do
     {:ok, session} = Wallaby.start_session
-    {:ok, session: session}
+    resized_session = resize_window(session, 1920, 1080)
+    {:ok, session: resized_session}
   end
 end
 
--- apps/web_agent/test/test_helper.exs
+++ apps/web_agent/test/test_helper.exs
@@ -1,3 +1,14 @@
 ExUnit.start()
-
 {:ok, _} = Application.ensure_all_started(:wallaby)
+Application.put_env(:wallaby, :screenshot_on_failure, true)
+
+defmodule WebAgent.TestHelpers do
+
+  use Wallaby.DSL
+
+  import Wallaby.Query, only: [css: 1]
+
+  def get_text(session, selector) do
+    session |> find(css(selector)) |> Element.text
+  end
+end
