6d4f764a9d52ed0a9a0a5d522404f52a73989abf
anonymous <anonymous>
2017-12-24 21:53:02 +0000

    a way to install webagent custom

--- apps/web_agent/test/support/integration_case.ex
+++ apps/web_agent/test/support/integration_case.ex
@@ -11,7 +11,15 @@ defmodule WebAgent.IntegrationCase do
   end
 
   setup do
-    {:ok, session} = Wallaby.start_session
+    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
+    accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+    connection = "keep-alive"
+
+    custom_headers = [{:accept, accept},
+                      {:connection, connection},
+                      {:user_agent, user_agent}]
+
+    {:ok, session} = Wallaby.start_session([{:custom_headers, custom_headers}, {:user_agent, user_agent}])
     resized_session = resize_window(session, 1920, 1080)
     {:ok, session: resized_session}
   end
