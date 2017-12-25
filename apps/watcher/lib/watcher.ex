56073dd15c2048f2d2651f9fdb6e84ba3b2acd5e
anonymous <anonymous>
2017-12-25 04:16:40 +0000

    mix run lib/watcher.ex will wait for a new file created in a directory and announce it

--- apps/watcher/lib/watcher.ex
+++ apps/watcher/lib/watcher.ex
@@ -1,20 +1,13 @@
 defmodule Watcher do
-  use Application
-
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
-  def start(_type, args) do
-    import Supervisor.Spec, warn: false
-
-    # Define workers and child supervisors to be supervised
-    children = [
-      # Starts a worker by calling: Watcher.Worker.start_link(arg1, arg2, arg3)
-      # worker(Watcher.Worker, [args[:watcher_name], args[:paths]]),
-    ]
-
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Watcher.Supervisor]
-    Supervisor.start_link(children, opts)
+  def watch do
+    IO.puts "Watching..."
+    {:ok, _pid} = FileSystem.start_link(dirs: ["../web_agent/screenshots"], name: :my_monitor_name)
+    FileSystem.subscribe(:my_monitor_name)
+    receive do
+      x -> IO.inspect x
+    end
+    IO.puts "Found one!"
   end
 end
+
+Watcher.watch
