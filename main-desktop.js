1cf060544965f9e39110961e3aeb843367c31fb7
anonymous <anonymous>
2018-01-26 23:13:11 -0500

    desktop version starts with npm start in one terminal and npm run electon-start in another

---
+++ main-desktop.js
@@ -0,0 +1,89 @@
+const electron = require('electron')
+// Module to control application life.
+const app = electron.app
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+// put this in configs
+var wss = 'wss://xxx.xx.x.xxx'
+
+app.on('certificate-error', (event, webContents, siteurl, error, certificate, callback) => {
+  var slice = siteurl.substr(0,18)
+  var slice_two = wss.substr(0,18)
+  if (slice === slice_two) {
+    console.log('verified')
+    // Verification logic.
+    event.preventDefault()
+    callback(true)
+  } else {
+    callback(false)
+  }
+})
+
+
+function createWindow () {
+  // Create the browser window.
+
+  // const subex = require('child_process').spawn('../server/rel/mychat/bin/mychat', ['start'])
+
+  // var rq = require('request-promise')
+  var mainAddr = 'http://localhost:4000'
+
+  var startUp = function(){
+/*
+    rq(mainAddr)
+      .then(function(htmlString){
+        console.log('server started!')
+
+        // TODO: get elixir to start more smoothly
+        // const subex = require('child_process').spawn('../server/rel/mychat/bin/mychat', ['restart'])
+
+*/
+        mainWindow = new BrowserWindow({width: 800, height: 600, frame: false})
+        mainWindow.loadURL(`http://localhost:3000/`)
+        mainWindow.on('closed', function () {
+          // put an array if your app supports multi windows
+          mainWindow = null
+        })
+/*
+      })
+      .catch(function(err){
+        console.log('waiting for the server start...')
+        console.error(err)
+        startUp()
+      })
+*/
+  }
+
+  // fire!
+  startUp()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
