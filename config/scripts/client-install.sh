5acbba48d0de299d623eec3ff1d830576f412ce8
anonymous <anonymous>
2018-02-01 03:08:47 -0500

    changed back to create-react-app but must apply patch to get directories to work

--- config/scripts/client-install.sh
+++ config/scripts/client-install.sh
@@ -8,3 +8,8 @@ sudo apt-get install -y node-gyp g++
 # four argument function stops compile. needs to be made to 3
 # _ => panic! (or similar) needed on pattern match
 
+# create-react-app needs config/scripts/create-react-app.patch applied from node_modules/react-scripts directory with 
+# (first copy config/scripts/create-react-app.patch to node_modules/react-scripts)
+# 'git apply create-react-app.patch'
+
+# install node v8.9.4 as default
