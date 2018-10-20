a8efc7a66f69a0b70f8d02c87b822b31f06b0997
anonymous <you@example.com>
2018-10-20 17:36:26 +0000

    server handler for fixed sync

--- apps/datomic/lib/datomic/channel.ex
+++ apps/datomic/lib/datomic/channel.ex
@@ -18,8 +18,17 @@ defmodule Datomic.Channel do
   def sync(latest_tx, subscription, socket) do
     %{topic: topic} = socket
 
+    IO.puts "latest tx"
+    IO.inspect latest_tx
+
+    latest_tx = cond do
+      latest_tx == "none" ->
+        1
+      true ->
+        String.to_integer(latest_tx)
+    end
+
     datomicsubscription = Enum.map(subscription, fn(x) ->
-      latest_tx = String.to_integer(latest_tx)
       {:ok, reversiblequery} = DatomicQueryTranslator.translatetodatoms(Enum.at(x,0))
       query = Exdn.from_elixir! reversiblequery
       {:ok, edn} = DatomicGenServer.q(via_tuple(topic), query, [ Exdn.from_elixir!({:list, [{:symbol, :"datomic.api/since"}, {:symbol, :"datomic_gen_server.peer/*db*"}, latest_tx ]})], [:options, {:client_timeout, 100_000}])
@@ -34,7 +43,14 @@ defmodule Datomic.Channel do
     end)
     |> Enum.reverse
 
-    final_tx = Enum.at(Enum.at(listfrommapset, 0), 3)
+    IO.inspect listfrommapset
+
+    final_tx = cond do
+      listfrommapset == [] ->
+        'none'
+      true ->
+        Enum.at(Enum.at(listfrommapset, 0), 3)
+    end
 
     {:ok, superfinaloutput} = Exdn.from_elixir finaloutput
     Datomic.TxForm.parse(superfinaloutput)
@@ -50,44 +66,6 @@ defmodule Datomic.Channel do
     |> Enum.group_by(fn x ->
       x["tx"]
     end)
-  end
-
-  def sync("none", subscription, socket) do
-    Logger.debug "MSG FALSE"
-    %{topic: topic} = socket
-
-    datomicsubscription = Enum.map(subscription, fn(x) ->
-      {:ok, reversiblequery} = DatomicQueryTranslator.translatetocurrentstate(Enum.at(x,0))
-#      {:ok, reversiblequery} = DatomicQueryTranslator.translatetodatoms(Enum.at(x,0))
-      query = Exdn.from_elixir! reversiblequery
-      {:ok, edn} = DatomicGenServer.q(via_tuple(topic), query, [], [:options, {:client_timeout, 100_000}])
-      Exdn.to_reversible edn
-    end)
-
-    finaloutput = RecUnion.recursiveunion(datomicsubscription)
-
-    finaloutput = MapSet.new(finaloutput, fn(x) ->
-      x ++ [1, true]
-    end)
-
-    {:ok, finaloutput} = Exdn.from_elixir finaloutput
-
-#    {:error, edn} = DatomicGenServer.q(DatomicGenServerLink, query, [], [:options, {:client_timeout, 100_000}])
-#    {:ok, edn} = DatomicGenServer.q(via_tuple(topic), query, [], [:options, {:client_timeout, 100_000}])
-    # IO.inspect is_binary(edn), label: "edn returns a string"
-#    Logger.debug fn -> edn end
-
-#    _grouped_tx = Datomic.TransactionLogQueryLogger.parse(finaloutput) |> Enum.group_by( fn(x) -> x["tx"] end )
-    Datomic.TxForm.parse(finaloutput) |> Enum.group_by(fn x ->
-      x["tx"]
-    end)
-  end
-
-  def sync(latest_tx, _socket) do
-    query = "[:find ?e ?a ?v ?tx ?op :in ?log ?t1 :where [(tx-ids ?log ?t1 nil) [?tx ...]] [(tx-data ?log ?tx) [[?e ?a ?v _ ?op]]]]"
-    {:ok, edn} = DatomicGenServer.qlog(DatomicGenServerLink, query, latest_tx - 1 , [], [:options, {:client_timeout, 100_000}])
-    IO.puts "test"
-    _grouped_tx = Datomic.TransactionLogQueryLogger.parse(edn) |> Enum.group_by( fn(x) -> x["tx"] end )
   end
 end
 
