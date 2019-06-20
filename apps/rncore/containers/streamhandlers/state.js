1c611383c713343bed0a278a6135daf10bd0456f
Someone <someone@somewhere.com>
2019-06-20 06:36:37 -0400

    reverts back to universal observable config

--- apps/rncore/containers/streamhandlers/state.js
+++ apps/rncore/containers/streamhandlers/state.js
@@ -1,18 +1,8 @@
-import { from, of, Observable } from 'rxjs'
 import { switchMap, map, startWith, tap, catchError } from 'rxjs/operators'
 import {
-  mapPropsStreamWithConfig,
-  createEventHandlerWithConfig
+  mapPropsStream,
+  createEventHandler
 } from 'recompose'
-import { isEmpty } from 'lodash'
-
-const rxjsconfig = {
-  fromESObservable: config => new Observable(config.subscribe),
-  toESObservable: stream => stream
-}
-
-const mapPropsStream = mapPropsStreamWithConfig(rxjsconfig)
-const createEventHandler = createEventHandlerWithConfig(rxjsconfig)
 
 const context = mapPropsStream(props$ => {
   const { stream: state$, handler: stateSelect } = createEventHandler()
